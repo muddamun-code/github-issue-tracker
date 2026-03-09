a2ce4db31c3863b327e89650666c11f178dc9b31
muddamun-code <graphicsdexterity17@gmail.comm>
2026-03-09 22:26:55 +0600

    init project

---
+++ README.md
@@ -0,0 +1,6 @@
+1. Difference between var, let and const
+
+ ans : 
+var is the old way to declare variables in JavaScript. It can be redeclared and updated.
+let is a newer way and it cannot be redeclared in the same scope but it can be updated.
+const is used when we do not want to change the value of the variable. Once assigned, it cannot be updated.
---
+++ login.js
@@ -0,0 +1,13 @@
+function login(){
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if(username === "admin" && password === "admin123"){
+        localStorage.setItem("login", "true");
+        window.location.href = "dashboard.html";
+    }else{
+        alert("Invalid username or password");
+    }
+
+}
